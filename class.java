3d49b50cdd23cda53abc52ef2347e4c90ee85bac
engineern2019 <alidawood15@gmail.com>
2019-06-03 11:44:45 +0100

    "first commit"

---
+++ class.java
@@ -0,0 +1,20 @@
+class results{
+    public static void main(String xys[]){
+        int phy, che, mat, total;
+        flout per;
+        phy=50
+        che=100;
+        mat=100;
+        total=phy+che+mat;
+        per=total*100/450;
+        system.out.println("total marks: "+total);
+        system.out.println("percentage: "+per);
+        if(per>=60){
+            system.out.println("You have passed the exam");
+            {
+                if(per<=)
+            system.out.println("You have failed the exam");
+            }
+        }
+    }
+}
---
+++ loops1.java
@@ -0,0 +1,10 @@
+class loops1{
+     public static void main(String xys[]){
+        int A;
+        A=1;
+        while(A<=10){
+            System.out.println(A);
+            A+=1;
+        }
+    }
+}
