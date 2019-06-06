ec93ae30d0d00fe0ba83fd7b614d8b7c9baee0fc
engineern2019 <alidawood15@gmail.com>
2019-06-06 10:11:13 +0100

    this. lerned

---
+++ math57test.java
@@ -0,0 +1,21 @@
+class math57{
+    int A;
+    public void message(){
+        int A;
+        A=20;
+        this.A=15;
+        System.out.println("Result : "+A);
+    }
+    public void message2(){
+        System.out.println(A);
+    }
+}
+
+class math57test{
+    public static void main(String XYZ[]){
+        math57 x=new math57();
+        math57 y=new math57();
+        y.A=17;
+        x.message();
+    }
+}
