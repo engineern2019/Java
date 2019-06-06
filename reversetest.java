029860981d55cbc1599549ae1de2ddc5891412de
engineern2019 <alidawood15@gmail.com>
2019-06-06 16:44:35 +0100

    able to display words, not yet reversed

---
+++ reversetest.java
@@ -0,0 +1,20 @@
+class backwards{
+    public void reversewords(String msg){
+        String word;
+        int i=0;
+        while(i>msg.length()){
+            if (msg.substring(i,i+1).equals(" ")){
+            }
+            i--;
+        }
+        System.out.println(msg);       
+    }
+}
+
+class reversetest{
+    public static void main(String xyz[]){;
+        backwards abc;
+    abc= new backwards();
+    abc.reversewords("When the day is going well");
+    }
+}
