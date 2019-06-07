f13ddd01231bb1f2bb4cbb500e3d434cf06be971
engineern2019 <alidawood15@gmail.com>
2019-06-07 17:01:52 +0100

    Interface import awt

---
+++ windows7.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+
+class Windows7{
+    public static void main(String xyz[]){
+        Frame win= new Frame();
+        //Color win= new Color();
+        Button B1= new Button("Click 1");
+        Button B2= new Button("Click 2");
+        Button B3= new Button("Click 3");
+        Button B4= new Button("Click 4");
+        Button B5= new Button("Click 5");
+        Action1 F= new Action1();
+        B1.addActionlistener(F);
+        //win.setBackground(Color.GREEN);
+        win.add(B1,BorderLayout.NORTH);
+        win.add(B1,BorderLayout.EAST);
+        win.add(B1,BorderLayout.SOUTH);
+        win.add(B1,BorderLayout.WEST);
+        win.add(B1,BorderLayout.CENTER);
+        win.setSize(400,400);
+        win.setVisible(true);
+    }
+}
+
+class action1 impliments Actionlistioner{
+    public void actionperformed(ActionEvent x){
+        System.out.prinln("Greetings")
+    }
+}
