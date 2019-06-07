77c7e564d87e9abde99fb848a5a656f198c60bc4
engineern2019 <alidawood15@gmail.com>
2019-06-07 11:16:14 +0100

    String Array, User Command

---
+++ Array2.java
@@ -0,0 +1,7 @@
+class Array2{
+    public static void main(String xys[]){
+        int a[]={10,12,90,80};
+        for(int i=0;i<a.length;i++)
+        System.out.println(a[i]);
+    }
+}
---
+++ StringArray.java
@@ -0,0 +1,8 @@
+class StringArray{
+    public static void main(String xyz[]){
+        int a;
+        for(a=0;a<xyz.length;a++){
+        System.out.println(xyz[a]);
+        }
+    }
+}
